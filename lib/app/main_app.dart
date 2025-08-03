0e2e66785c25bf8cff6f6796d6c0283707dc2a6b
Aditya Andar Rahim <adityandar@gmail.com>
2025-08-03 10:15:08 +0700

    refactor: Move MainApp to app folder

---
+++ lib/app/main_app.dart
@@ -0,0 +1,45 @@
+import 'package:badmintoon/core/core.dart';
+import 'package:badmintoon/dependencies/dependencies.dart';
+import 'package:badmintoon/routers/index.dart';
+import 'package:badmintoon/shared/shared.dart';
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  MainApp({super.key});
+
+  final _bdRouter = BdRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => GameplayCubit(),
+      child: MaterialApp.router(
+        routerConfig: _bdRouter.config(),
+        title: 'Badmintoon',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: BdColors.primary),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        builder: (context, child) {
+          return LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              // Add width condition of Web
+              if (constraints.maxWidth > 600) {
+                return Scaffold(
+                  body: Center(
+                    child: Text(
+                      'Maaf, website ini hanya bisa diakses melalui handphone',
+                    ),
+                  ),
+                );
+              } else {
+                return child ?? const SizedBox.shrink();
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:badmintoon/core/core.dart';
+import 'package:badmintoon/dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
 
-import 'main_app.dart';
+import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: HydratedStorageDirectory.web,
+  );
+  BdServiceLocator.init();
   runApp(MainApp());
 }
